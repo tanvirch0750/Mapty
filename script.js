992cc2ca63b3d282a1cf475a74c7d4acde4bfcbe
tanvirch0750 <tanvir0750@yahoo.com>
2021-03-02 20:22:32 +0600

    Display map using a third part library called leaflet

--- script.js
+++ script.js
@@ -16,7 +16,21 @@ if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(
     function (position) {
       const { latitude, longitude } = position.coords;
+      const coords = [latitude, longitude];
+      //TEST
       console.log(`https://www.google.com/maps/@${latitude},${longitude},14z`);
+      // display map using a third party library called leaflet.
+      const map = L.map('map').setView(coords, 13);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
     },
     function () {
       alert('Could not get your location');
