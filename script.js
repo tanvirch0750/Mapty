dd2cf5067f9ab9eb3c1eb110747ec1e12b4b5fee
tanvirch0750 <tanvir0750@yahoo.com>
2021-03-10 14:18:30 +0600

    Move to marker on click

--- script.js
+++ script.js
@@ -73,6 +73,7 @@ class App {
     this._getPositions();
     form.addEventListener('submit', this._newWorkout.bind(this));
     inputType.addEventListener('change', this._toggleElevationField.bind(this));
+    containerWorkouts.addEventListener('click', this._moveToPopup().bind(this));
   }
 
   _getPositions() {
@@ -249,6 +250,16 @@ class App {
       `;
     }
     form.insertAdjacentHTML('afterend', html);
+  }
+
+  _moveToPopup(e) {
+    const workoutEl = e.target.closest('.workout');
+
+    if (!workoutEl) return;
+
+    const workout = this.#workouts.find(
+      workout => workout.id === workoutEl.dataset.id
+    );
   }
 }
 
